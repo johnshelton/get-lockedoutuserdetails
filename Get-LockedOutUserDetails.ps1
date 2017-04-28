98fe801fffd1194009b87d784e5082dfba996e9e
John Shelton <john.shelton@lucky13solutions.com>
2017-04-28 16:25:07 -0400

    Changed output to HTML.  Added Progress Bar.

--- Get-LockedOutUserDetails.ps1
+++ Get-LockedOutUserDetails.ps1
@@ -2,7 +2,7 @@
 # Get-LockedOutUserDetails
 # Created on: 2017-01-04
 # Version 1.0
-# Last Updated: 
+# Last Updated: 2017-04-28
 # Last Updated by: John Shelton | c: 260-410-1200 | e: john.shelton@lucky13solutions.com
 #
 # Purpose: This script determines which AD server has the PDC role.  It then reviews the
@@ -12,7 +12,7 @@
 #          and the ComputerName that was using the credentials.
 # Notes: 
 # 
-# Change Log:
+# Change Log: Feature 1:  Output to HTML
 # 
 #
 #=======================================================================================
@@ -21,11 +21,39 @@
 #
 $LockedUser = ""
 [array] $LockEvents
+[array] $Events
+#
+# Configure HTML Header
+#
+$HTMLHead = "<style>"
+$HTMLHead += "BODY{background-color:white;}"
+$HTMLHead += "TABLE{border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}"
+$HTMLHead += "TH{border-width: 1px;padding: 0px;border-style: solid;border-color: black;background-color:royalblue}"
+$HTMLHead += "TD{border-width: 1px;padding: 0px;border-style: solid;border-color: black;background-color:gainsboro}"
+$HTMLHead += "</style>"
+#
+# Define Output Variables
+#
+$ExecutionStamp = Get-Date -Format yyyyMMdd_hh-mm-ss
+$path = "c:\temp\"
+$FilenamePrepend = 'temp_'
+$FullFilename = "Get-LockedOutUserDetails.ps1"
+$FileName = $FullFilename.Substring(0, $FullFilename.LastIndexOf('.'))
+$FileExt = '.html'
+$OutputFile = $path + $FilenamePrePend + '_' + $FileName + '_' + $ExecutionStamp + $FileExt
+#
+$PathExists = Test-Path $path
+IF($PathExists -eq $False)
+    {
+    New-Item -Path $path -ItemType  Directory
+    }
+#
 # Enable setting the LockedUser via a param
 #
 param (
     [string]$LockedUser = $(Read-Host "Input the Username who is locked")
     )
+$LockedUser = $LockedUser.ToUpper()
 #
 # Clear the screen
 #
@@ -40,16 +68,29 @@ Clear-Host
 # Determine the AD Server with the PDCEmulator role enabled
 #
 $PDC = Get-ADDomain | Select-Object PDCEmulator
+$PDCName = $PDC.PDCEmulator
+Write-Host "We have determined that $PDCName is the PDC for the environment"
 #
 # Get all of the Events from the Security Log with Event ID 4740
 #
-$Events = Get-WinEvent -ComputerName $PDC.PDCEmulator -FilterHashtable @{Logname='Security';Id=4740}
+Write-Host "We are now gathering Security Event Logs from the PDC...this may take a while..."
+# $Events = Get-WinEvent -LogName "Security" -ComputerName $PDCName
+$Events = Get-WinEvent -ComputerName $PDC.PDCEmulator -FilterHashtable @{Logname='Security';Id=4740} | Sort-Object -Property TimeCreated
+$EventsCount = $Events.Count
+$OldestEvent = $Events.TimeCreated | Select-Object -First 1
+$NewestEvent = $Events.TimeCreated | Select-Object -Last 1
+Write-Host "Found a total of $EventsCount lockout events"
 #
 # Search through all of the sorted Events for ones that match the specified UserName
 #
-$LockEvents = foreach ($event in $Events)
+$Progress = 0
+$LockEvents = @()
+ForEach ($event in $Events)
     {
-    $Event | Where-Object{$_.Properties[0].Value -like $LockedUser} | Select-Object -Property TimeCreated, @{Label='UserName'; Expression={$_.Properties[0].Value}},@{Label='ComputerName';Expression={$_.Properties[1].Value}}
+    $UserLockEvent = $Event | Where-Object{$_.Properties[0].Value -like $LockedUser} | Select-Object -Property TimeCreated, @{Label='UserName'; Expression={$_.Properties[0].Value}},@{Label='ComputerName';Expression={$_.Properties[1].Value}}; $I = $I+1
+    Write-Progress -Activity "Searching Events" -Status "Progress:" -PercentComplete (($Progress/$EventsCount)*100)
+    $Progress ++
+    $LockEvents += $UserLockEvent
     }
 If ($LockEvents.count -lt 1) 
     {
@@ -58,5 +99,9 @@ If ($LockEvents.count -lt 1)
     }
 Else 
     {
-    $LockEvents | Out-GridView
+    # $LockEvents | Out-GridView
+    # $LockEvents | ConvertTo-HTML -head $HTMLHead &45;body "<H2> Lockout Events for $LockedUser" | Invoke-Item
+    Write-Host "The Results will be written to $OutputFile"
+    $LockEvents | ConvertTo-HTML TimeCreated,UserName,ComputerName -Title "Lock Events for $LockedUser" -body "$HTMLHead<H2> Lock Events for $LockedUser </H2> </P> The oldest event in the log is dated $OldestEvent </BR> The most recent event is dated $NewestEvent </P>" | Set-Content $OutputFile
+    Invoke-Item $OutputFile
     }
